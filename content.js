390749a61419dfad78df03fb970b238274e78cbf
Achraf-Assal <achrafassa.le4@gmail.com>
2024-11-19 15:21:50 +0100

    removing the addlistener from content.js bcs content.js only for tab scripting

--- content.js
+++ content.js
@@ -1,25 +1,9 @@
-// takes the screenshot url and downs it or displays it
-const screenshotBtn = document.querySelector(".shotBtn");
+const pageHeight = document.documentElement.scrollHeight;
+const displayHeight = window.innerHeight;
 
-screenshotBtn.addEventListener("click", () => {
-  console.log("clicked");
-  chrome.runtime.sendMessage({ action: "takeScreenshot" }, (response) => {
-    if (response.imageUrl) {
-      console.log("Url: ", response.imageUrl);
-      //download img
-      const downloadLink = document.createElement("a");
-      downloadLink.href = response.imageUrl;
-      downloadLink.download = "Screenshot.png";
-      downloadLink.click();
-
-      //display img
-      const imgTag = document.createElement("img");
-      imgTag.src = response.imageUrl;
-      imgTag.style.width = "40px";
-      imgTag.style.height = "40px";
-      document.body.appendChild(imgTag);
-    } else {
-      console.error(response.farewell);
-    }
-  });
+// Send height information to the background script
+chrome.runtime.sendMessage({
+  action: "takeScreenshot",
+  Full_Height: pageHeight,
+  Display_Height: displayHeight,
 });
