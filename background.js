cfa6b5afb9b1e7c4e4d13232f0a8c9b2f71a16d7
Achraf-Assal <achrafassa.le4@gmail.com>
2024-11-19 15:56:40 +0100

    the background.js code is not working bcs of the window.scroll , i am just pushing this

--- background.js
+++ background.js
@@ -1,28 +1,51 @@
-// handles logic to screenshot webpage
+
+function CaptureVisibility (imageList){
+  chrome.tabs.captureVisibleTab(null, { format: "png" }, (imageUrl) => {
+    if (chrome.runtime.lastError) {
+      console.error(
+        "Error capturing screenshot:",
+        chrome.runtime.lastError.message
+      );
+      sendResponse({
+        farewell: "Failed to take screenshot",
+        error: chrome.runtime.lastError.message,
+      });
+    } else if (!imageUrl) {
+      console.error("Screenshot capture returned undefined URL.");
+      sendResponse({
+        farewell: "Screenshot capture returned undefined URL.",
+      });
+    } else {
+      console.log("Screenshot URL:", imageUrl);
+      // sendResponse({ imageUrl: imageUrl });
+      imageList.push(imageUrl)
+    }
+  });
+
+} 
+
+function MultipleScreenShot(scrollCount,Display_Height) {
+  var currentTop = 0
+  var imageList = [];
+  var currentBottom = Display_Height;
+  for (let index = 0; index < scrollCount; index++) {
+    window.scroll(currentTop,currentBottom)
+    CaptureVisibility(imageList)
+    currentTop = currentBottom;
+    currentBottom += Display_Height;
+  }
+  console.log(imageList);
+  
+}
+
 
 chrome.runtime.onMessage.addListener((message, _sender, sendResponse) => {
   if (message.action === "takeScreenshot") {
-    chrome.tabs.captureVisibleTab(null, { format: "png" }, (imageUrl) => {
-      if (chrome.runtime.lastError) {
-        console.error(
-          "Error capturing screenshot:",
-          chrome.runtime.lastError.message
-        );
-        sendResponse({
-          farewell: "Failed to take screenshot",
-          error: chrome.runtime.lastError.message,
-        });
-      } else if (!imageUrl) {
-        console.error("Screenshot capture returned undefined URL.");
-        sendResponse({
-          farewell: "Screenshot capture returned undefined URL.",
-        });
-      } else {
-        console.log("Screenshot URL:", imageUrl);
-        sendResponse({ imageUrl: imageUrl });
-      }
-    });
-    // async
+    console.log("Full Page Height:", message.Full_Height);
+    console.log("Full Page Height:", message.Display_Height);
+    const scrollCount = Math.ceil(message.Full_Height / message.Display_Height);
+    console.log("times to scroll :", scrollCount);
+    MultipleScreenShot(scrollCount,message.Display_Height)
     return true;
   }
 });
